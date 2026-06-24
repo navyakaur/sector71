99d4fbf65f9faa85be2d21ef0f6c8819ae6c4309
navyakaur <navyak@gmail.com>
2026-06-23 20:16:04 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import Header from './components/Header.jsx'
 import './index.css'
+import Header from './components/Header'
+import Home from './pages/Home'
+import About from './pages/About'
+import AboutSector71 from './pages/AboutSector71'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -14,7 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/about-sector71" element={<AboutSector71 />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
